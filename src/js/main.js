3501753721773e334b8bdcfc9c08d51b0bcf4a67
Nastya <nastya.ekamasova2014@gmail.com>
2026-03-30 16:10:59 +0300

    add js

---
+++ src/js/main.js
@@ -0,0 +1,131 @@
+window.addEventListener('DOMContentLoaded', function() {
+    
+	let tabs = document.querySelectorAll('.tabheader__item'),
+		tabsContent = document.querySelectorAll('.tabcontent'),
+		tabsParent = document.querySelector('.tabheader__items');
+
+	function hideTabContent() {
+        
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+	}
+
+	function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+    
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if(target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+
+    //timer
+    const deadline = "2026-05-11";
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+              days = Math.floor (t/ (1000*60*60*24)),
+              hours = Math.floor ((t/ (1000*60*60) % 24)),
+              minutes = Math.floor ((t/ 1000 / 60) % 60),
+              seconds = Math.floor ((t/ 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function setZero (num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock (selector, endtime) {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+        
+        function updateClock () {
+            const t = getTimeRemaining(endtime);
+
+              if (t.total <= 0) {
+                clearInterval(timeInterval);
+                days.innerHTML = 0;
+                hours.innerHTML = 0;
+                minutes.innerHTML = 0;
+                seconds.innerHTML = 0;
+                return;
+            }
+
+            days.innerHTML = setZero(t.days);
+            hours.innerHTML = setZero(t.hours);
+            minutes.innerHTML = setZero(t.minutes);
+            seconds.innerHTML = setZero(t.seconds);
+        }
+    }
+
+    setClock('.timer', deadline);
+
+    //modal window
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+        modalWindow = document.querySelector('.modal'),
+        modalCloseBtn = document.querySelector('[data-close]');
+
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', () => {
+            modalWindow.classList.add('show');
+            modalWindow.classList.remove('hide');
+            document.body.style.overflow = 'hidden';
+        });
+    });
+   
+    function closeModal () {
+        modalWindow.classList.add('hide');
+        modalWindow.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+    modalCloseBtn.addEventListener('click', closeModal);
+
+    modalWindow.addEventListener('click', (e) => {
+        if (e.target === modalWindow) {
+            closeModal ();
+        }
+    });
+
+    document.addEventListener ('keydown', (e) => {
+        if(e.code === 'Escape' && modalWindow.classList.contains('show')) {
+            closeModal();
+        }
+    });
+
+});
