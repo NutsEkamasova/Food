9b314943d8d3b6475ce0f2d5efe1db79155d9dfc
Nastya <nastya.ekamasova2014@gmail.com>
2026-04-02 14:23:41 +0300

    добавление карточек через классы

--- dist/js/script.js
+++ dist/js/script.js
@@ -140,5 +140,64 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 
         window.addEventListener('scroll', showModalByScroll);
+
+         //использование классов
+        class Card {
+            constructor(src, alt, tittle, descr, price, parentSelector){
+                this.src=src;
+                this.alt=alt;
+                this.tittle=tittle;
+                this.descr=descr;
+                this.price=price;
+                this.parent=document.querySelector(parentSelector);
+                this.transfer=27;
+                this.changeToUAH();
+            }
+            changeToUAH() {
+                this.price=this.price*this.transfer;
+            }
+            render() {
+                const element= document.createElement('div');
+                element.innerHTML=`
+                <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.tittle}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+                `;
+                this.parent.append(element);
+            }
+        }
+
+        new Card(
+            "img/tabs/vegy.jpg",
+            "vegy",
+            'Меню "Фитнес"',
+            'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+            9,
+            '.menu__field .container'
+        ).render();
+
+        new Card(
+            "img/tabs/elite.jpg",
+            "elite",
+            'Меню “Премиум”',
+            'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+            14,
+            '.menu__field .container'
+        ).render();
+
+        new Card(
+            "img/tabs/post.jpg",
+            "post",
+            'Меню "Постное"',
+            'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+            21,
+            '.menu__field .container'
+        ).render();
     });
  
